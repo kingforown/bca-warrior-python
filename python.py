923617538351f0f7af21d54b32b5c0b36f273dbe
Abinesh <119767173+kingforown@users.noreply.github.com>
2024-12-01 11:06:38 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+a=1
+b=1.1
+c="abi"
+d=True
+
+print("This is Your Data Type :",type(a))
+print("This is Your Data Type :",type(b))
+print("This is Your Data Type :",type(c))
+print("This is Your Data Type :",type(d))
+
