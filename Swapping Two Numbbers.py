77a2b9aa767ca6aee3bfb1e0ba05fab8fbc04ee4
Abinesh <119767173+kingforown@users.noreply.github.com>
2024-12-12 00:36:15 -0800

    Create Swapping Two Numbbers.py

---
+++ Swapping Two Numbbers.py
@@ -0,0 +1,20 @@
+# Swapping Two Numbbers
+# Example Input: 
+    # Enter The First Number  : 10
+    # Enter The Second Number : 20
+
+#source code ~
+
+a=int(input("Enter The First Number  :"))
+b=int(input("Enter The Second Number :"))
+
+temp = a
+a = b
+b = temp
+print("Your Swapped First Number  ↓↑  :",a)
+print("Your Swapped Second Number ↓↑  :",b)
+
+
+#Output:
+# Your Swapped First Number  ↓↑  : 20
+# Your Swapped Second Number ↓↑  : 10
